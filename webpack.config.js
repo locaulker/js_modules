ebaf39b99994b8f7188c66a3d54c152ae410d0f4
Lawrence <locaulker@gmail.com>
2017-02-27 06:27:38 -0500

    Initiated Babel Integration

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
 const path = require('path');
+// path is a webpack object that has a method called resolve()
 
 const config = {
   entry: './src/index.js', // Relative Path
   output: {
-    path: path.resolve(__dirname, 'build'), // Absolute Path
+    path: path.resolve(__dirname, 'build'),
+    // the resolve() function determines the Absolute Path
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        // Reg Expression to check if file ends with .js
+        test: /\.js$/
+      }
+    ]
   }
 };
 
